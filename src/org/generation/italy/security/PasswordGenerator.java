791ce246fec2c62623210dd49c3c769390334a51
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-15 14:41:31 +0100

    secondo commit

--- src/org/generation/italy/security/PasswordGenerator.java
+++ src/org/generation/italy/security/PasswordGenerator.java
@@ -4,6 +4,38 @@ public class PasswordGenerator {
 
 	public static void main(String[] args) {
 		
+		String name;
+		String surname;
+		String favColor;
+		
+		int bdayDay;
+		int bdayMonth;
+		int bdayYears;
+		
+		
+		//assegnamento
+		
+		name = "Elia";
+		surname = " Mazzucco";
+		favColor = "Purple";
+		
+		bdayDay = 16;
+		bdayMonth = 8;
+		bdayYears = 1995;
+		
+		String bDay = "16-08-1995";
+		int bDayTot = bdayDay + bdayMonth + bdayYears;
+		
+		
+		System.out.println("Ho un utente che si chiama " + name + surname + " nato il " + bDay + " il cui colore preferito è il " + favColor);
+		System.out.print("La sua password sarà" name + "-" + surname + "-" + favColor + "-" );
+		System.out.println(bDayTot);
+		
+		
+		
+	
+		
+				
 
 	}
 
